b4320da3958b9de4c42904f9bd2547b2f072b2e2
Spike2250 <shilov.spike2250@yandex.ru>
2024-04-05 01:08:10 +0500

    Update views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,6 +5,4 @@ from django.utils.translation import gettext as _
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        a = None
-        a.hello()
         return render(request, 'index.html', context={'text': _('Some text to test translations')})
