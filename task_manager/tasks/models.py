e9f7c0c8d52483fbea484eea5e5b38bb8f96c0f2
Spike2250 <shilov.spike2250@yandex.ru>
2024-05-14 21:46:13 +0500

    fix translate #4

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from task_manager.users.models import User
 from task_manager.labels.models import Label
@@ -8,30 +9,30 @@ from task_manager.statuses.models import Status
 class Task(models.Model):
     name = models.CharField(
         max_length=255, unique=True, blank=False,
-        verbose_name='Name',
+        verbose_name=_('Name'),
     )
     description = models.TextField(
         blank=True,
-        verbose_name='Description',
+        verbose_name=_('Description'),
     )
     author = models.ForeignKey(
         User, blank=False, on_delete=models.PROTECT,
         related_name='author',
-        verbose_name='Author',
+        verbose_name=_('Author'),
     )
     executor = models.ForeignKey(
         User, null=True, blank=True, default='', on_delete=models.PROTECT,
         related_name='executor',
-        verbose_name='Executor',
+        verbose_name=_('Executor'),
     )
     status = models.ForeignKey(
         Status, blank=False, on_delete=models.PROTECT,
         related_name='status',
-        verbose_name='Status',
+        verbose_name=_('Status'),
     )
     labels = models.ManyToManyField(
         Label, blank=True,
         related_name='labels',
-        verbose_name='Labels',
+        verbose_name=_('Labels'),
     )
     created_at = models.DateTimeField(auto_now_add=True)
